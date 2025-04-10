4ed6519a64008ebe24e39ae8ba31d7229ae200c8
maksEveline <maksimeveline@gmail.com>
2025-04-10 18:47:53 +0300

    finanal

--- handlers/user_commands.py
+++ handlers/user_commands.py
@@ -3,7 +3,7 @@ from aiogram.types import Message
 
 from data.database import add_user_if_not_exists
 from keyboards.user import get_main_menu
-from config import DB_PATH
+from config import DB_PATH, ADMIN_ID
 
 router = Router()
 
@@ -11,10 +11,12 @@ router = Router()
 @router.message(F.text == "/start")
 async def start_func(msg: Message):
     user_id = msg.from_user.id
-    await add_user_if_not_exists(DB_PATH, user_id)
 
-    await msg.answer(
-        text=f"Привет, <b>{msg.from_user.full_name}</b>",
-        parse_mode="html",
-        reply_markup=await get_main_menu(),
-    )
+    if user_id in ADMIN_ID:
+        await add_user_if_not_exists(DB_PATH, user_id)
+
+        await msg.answer(
+            text=f"Привет, <b>{msg.from_user.full_name}</b>",
+            parse_mode="html",
+            reply_markup=await get_main_menu(),
+        )
